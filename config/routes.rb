5d19728e1a13a37299189150d5c2403b8e34bee6
Shawn Wildo <shawnmallorywilliams@gmail.com>
2016-10-27 17:11:12 -0400

    Fixing rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about_us', to: 'pages#about_us'
+  root 'pages#home'
 end
